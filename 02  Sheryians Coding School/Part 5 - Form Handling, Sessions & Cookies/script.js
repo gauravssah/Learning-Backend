cb345c2c0c3c4dde035d84d173378f71a882f2ea
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-04-26 15:09:29 +0530

    coocco

---
+++ 02  Sheryians Coding School/Part 5 - Form Handling, Sessions & Cookies/script.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.send("Mai hu don....");
+});
+
+app.listen(3000);
