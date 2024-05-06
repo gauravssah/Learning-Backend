36fd28e5b498b6d1a1978cc13f19e533387c7eaf
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-05-06 16:03:51 +0530

    like and edit done

--- 02  Sheryians Coding School/Part 17 Mini Project 1 - Part 1/app.js
+++ 02  Sheryians Coding School/Part 17 Mini Project 1 - Part 1/app.js
@@ -5,6 +5,7 @@ const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
+const { render } = require("ejs");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -35,7 +36,7 @@ app.post("/register", async (req, res) => {
             let token = jwt.sign({ email: email, userid: user._id }, "shhhh");
             res.cookie("token", token);
 
-            res.send("User Registered.");
+            res.redirect("/profile");
 
         })
 
@@ -49,8 +50,35 @@ app.get("/login", (req, res) => {
 
 app.get("/profile", isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({ email: req.user.email }).populate("posts");
-
     res.render("profile", { user });
+});
+
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+    if (post.likes.indexOf(req.user.userid) === -1) {
+        post.likes.push(req.user.userid);
+
+    } else {
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+
+    await post.save();
+
+    res.redirect("/profile");
+});
+
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+
+    res.render("edit", { post });
+
+});
+
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+    let post = await postModel.findOneAndUpdate({ _id: req.params.id }, { content: req.body.content });
+
+    res.redirect("/profile");
 
 });
 
