92427226855f3fda9eef6344e4325beed2e721fa
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-05-19 23:57:54 +0530

    add subscription model

---
+++ 03 Chai or Backend/04 How to setup a professional backend project - YOUTUBE/src/models/subscription.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionScheme = new Schema(
+    {
+        subscriber: {
+            type: Schema.Types.ObjectId, // one who is subscriber
+            ref: "User",
+
+        },
+        channel: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionScheme);
