843c057394356aefdd1187caf1e3ae5a3ec2a175
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-05-21 23:51:06 +0530

    pipeline ageration

--- 03 Chai or Backend/04 How to setup a professional backend project - YOUTUBE/src/controllers/user.controller.js
+++ 03 Chai or Backend/04 How to setup a professional backend project - YOUTUBE/src/controllers/user.controller.js
@@ -324,6 +324,9 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
         }
     ).select("-password");
 
+    // TODO : Delete the old avatar 
+
+
     return res
         .status(200)
         .json(new ApiResponse(200, user, "Avatar updated successfully!"));
@@ -357,11 +360,89 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         }
     ).select("-password");
 
+    // TODO : Delete the old CoverImage 
+
     return res
         .status(200)
         .json(new ApiResponse(200, user, "Cover image updated successfully!"));
 
 });
+
+// -------- getUserChannelProfile --------
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+
+    const { username } = req.params;
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing!");
+    };
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {
+                            $in: [req.user?._id, "$subscribers.subscriber"]
+                        },
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+
+            }
+        }
+
+    ])
+
+    if (!channel?.length) {
+        throw new ApiError(404, "Channel does not exist!")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, channel[0], "User Channel fetched successfully!"));
+
+})
 
 export {
     registerUser,
