7b36c714f6923272bb8f431f591e26c75c8ea04b
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-08-26 10:52:32 +0530

    request and response

---
+++ 04 Backend Domination/03 Express/08 Request and Response/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    // This is response comming from server.
+    res.send('Understanding Request and Response');
+
+    // This is request comming from client, or browser.
+    console.log(req);
+    // console.log(req.body);
+    // console.log(req.cookies);
+    // console.log(req.query);
+    // console.log(req.ip);
+    // console.log(req.headers);
+    // console.log(req.url);
+    // console.log(req.method);
+
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000! http://localhost:3000');
+});
+
+
+
