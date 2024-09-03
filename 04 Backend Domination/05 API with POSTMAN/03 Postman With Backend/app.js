0142f750906c267e158cbf28fd25171887fbc7b6
gauravssah <124705806+gauravssah@users.noreply.github.com>
2024-09-03 17:20:03 +0530

    postman variables

--- 04 Backend Domination/05 API with POSTMAN/03 Postman With Backend/app.js
+++ 04 Backend Domination/05 API with POSTMAN/03 Postman With Backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 let data = [1, 2, 3, 4, 5];
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello Gaurav!");
 });
 
 app.get("/data", (req, res) => {
@@ -14,6 +14,16 @@ app.get("/data", (req, res) => {
 app.post("/data/:num", (req, res) => {
     data.push(parseInt(req.params.num));
     res.send(data);
+});
+
+// This is for testing purpose
+
+app.get("/api/testing/development", (req, res) => {
+    res.send("Hello Gaurav in development!");
+});
+
+app.get("/api/testing/development/user", (req, res) => {
+    res.send("Hello Gaurav in development user!");
 });
 
 app.listen(3000);
